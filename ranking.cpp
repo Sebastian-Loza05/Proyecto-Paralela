d344f1bb4a1f13e1f3498a22f16833ff7821fb35
Sebastian-Loza05 <sebastian.loza@utec.edu.pe>
2025-06-21 13:14:11 -0500

    v1 y v2 ranking sort

---
+++ ranking.cpp
@@ -0,0 +1,184 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <mpi.h>
+#include <cmath>
+
+using namespace std;
+
+void generate_unique_array(std::vector<int>& arr, int n) {
+  arr.resize(n);
+
+  std::vector<int> pool(n * 10);
+  std::iota(pool.begin(), pool.end(), 0);
+
+  // baraja aleatoriamente
+  std::mt19937 g(12345);
+  std::shuffle(pool.begin(), pool.end(), g);
+
+  // copia los primeros n valores únicos
+  std::copy(pool.begin(), pool.begin() + n, arr.begin());
+}
+
+void print_array(vector<int> arr, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+int main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  // Constants
+  int n = 28;
+  int P = sqrt(size);
+  int N = n / P;
+  int row = rank / P;
+  int col = rank % P;
+  MPI_Request requests[P];
+
+  // arrays
+  vector<int> global_arr(n, 0);
+  vector<int> local_row(N, 0);
+  vector<int> local_col(N, 0);
+
+  // Generate array and boradcast cols
+  if (rank == 0) {
+    generate_unique_array(global_arr, n);
+    cout << "Generated array: ";
+    print_array(global_arr, n);
+
+    for (int i = 1; i < P; i++)
+      MPI_Isend(&global_arr[i*N], N, MPI_INT, i, 0, MPI_COMM_WORLD, &requests[i]);
+
+    for (int i = 0; i < N; i++)
+      local_row[i] = global_arr[i];
+  }
+  
+  if ( rank > 0 && rank < P) {
+    MPI_Irecv(local_row.data(), N, MPI_INT, 0, 0, MPI_COMM_WORLD, &requests[rank]);
+    MPI_Wait(&requests[rank], MPI_STATUS_IGNORE);
+  }
+
+  if (rank < P) {
+    MPI_Send(local_row.data(), N, MPI_INT, rank + P, 0, MPI_COMM_WORLD);
+  }
+  
+  if (rank >= P && rank < size - P) {
+    MPI_Recv(local_row.data(), N, MPI_INT, rank - P, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Send(local_row.data(), N, MPI_INT, rank + P, 0, MPI_COMM_WORLD);
+  }
+
+  if (rank >= size - P) {
+    MPI_Recv(local_row.data(), N, MPI_INT, rank - P, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
+
+  // Print received rows
+  // for (int r = 0; r < size; r++) {
+  //   MPI_Barrier(MPI_COMM_WORLD);  // sincroniza antes de imprimir
+  //   if (rank == r) {
+  //       printf("Rank %d received row: ", rank);
+  //       for (int i = 0; i < N; i++) {
+  //           cout << local_row[i] << " ";
+  //       }
+  //       cout << endl;
+  //   }
+  // }
+
+  // Broadcast rows
+  MPI_Comm rowComm;
+  MPI_Comm_split(MPI_COMM_WORLD, row, col, &rowComm);
+
+  if (col == row) {
+    local_col = local_row;
+  }
+
+  MPI_Bcast(local_col.data(), N, MPI_INT, row, rowComm);
+  
+  // Print received cols
+  // for (int r = 0; r < size; r++) {
+  //   MPI_Barrier(MPI_COMM_WORLD);
+  //   if (rank == r) {
+  //       printf("Rank %d received col: ", rank);
+  //       for (int i = 0; i < N; i++) {
+  //           cout << local_col[i] << " ";
+  //       }
+  //       cout << endl;
+  //   }
+  // }
+
+  // Sort
+  sort(local_row.begin(), local_row.end());
+
+
+  // Local Ranking (promedio O(N^2))
+  vector<int> local_ranking(N, 0);
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (local_row[j] <= local_col[i])
+        local_ranking[i]++;
+      else
+        break;
+    }
+  }
+
+  // for (int r = 0; r < size; r++) {
+  //   MPI_Barrier(MPI_COMM_WORLD);  // sincroniza antes de imprimir
+  //   if (rank == r) {
+  //     printf("Rank %d: \n", rank);
+  //     for (int i = 0; i < N; i++) {
+  //       cout << local_row[i] << " ";
+  //     }
+  //     cout << endl;
+  //     for (int i = 0; i < N; i++) {
+  //       cout << local_col[i] << " ";
+  //     }
+  //     cout << endl;
+  //     for (int i = 0; i < N; i++) {
+  //       cout << local_ranking[i] << " ";
+  //     }
+  //     cout << endl;
+  //   }
+  // }
+
+  // Reduce local ranking
+  
+  vector<int> row_ranking(N, 0);
+  MPI_Reduce(local_ranking.data(),
+             row_ranking.data(),
+             N,
+             MPI_INT, MPI_SUM,
+             row,
+             rowComm
+             );
+
+  // Print reduced Ranking
+  for (int r = 0; r < size; r++) {
+    MPI_Barrier(MPI_COMM_WORLD);  // sincroniza antes de imprimir
+    if (rank == r && col == row) {
+      printf("Rank %d: \n", rank);
+      for (int i = 0; i < N; i++) {
+        cout << local_col[i] << " ";
+      }
+      cout << endl;
+      printf("Reduced ranking: ", rank);
+      for (int i = 0; i < N; i++) {
+        cout << row_ranking[i] << " ";
+      }
+      cout << endl;
+    }
+  }
+
+  MPI_Comm_free(&rowComm);
+
+  MPI_Finalize();
+  return 0;
+}
